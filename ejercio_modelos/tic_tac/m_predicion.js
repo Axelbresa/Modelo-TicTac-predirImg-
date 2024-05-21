9d86f12510c5c376992ba69e3aeee1573fd84f64
AxelBresa <bresanovichaxel43@gmail.com>
2024-05-20 23:02:25 -0300

    Se agrego el modelo de tic tac

--- ejercio_modelos/tic_tac/m_predicion.js
+++ ejercio_modelos/tic_tac/m_predicion.js
@@ -2,17 +2,12 @@ tf.ready().then(() => {
     const modelPath = './ttt_model.json'
     tf.tidy(() => {
       tf.loadLayersModel(modelPath).then((model) => {
-        // pasamos los tensores
-        const e = tf.zeros([9])
-        // const b = tf.tensor([-1, 0, 0, 1, 1, -1, 0, 0, -1])
-        // const g = tf.tensor([1, 0, 1, 0, -1, -1, -1, 0, 1])
+        const g = tf.tensor([1, 0, 0, 0, 0, 0, 0, 0 ,0])
 
-        // Stack states into a shape [3, 9]
-        const matches = tf.stack([e])
+        const matches = tf.stack([g])
         const result = model.predict(matches)
-        console.log(result)
         // Log the results
-        result.reshape([3, 3, 3]).print()
+        result.reshape([9]).print()
       })
     })
   })
--- ejercio_modelos/tic_tac/tic_tac.js
+++ ejercio_modelos/tic_tac/tic_tac.js
@@ -2,6 +2,8 @@ const cells = document.querySelectorAll('.cell');
 const resetButton = document.getElementById('resetButton');
 let currentPlayer = 'X';
 let board = ['', '', '', '', '', '', '', '', ''];
+let g = tf.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+g.print()
 
 const winningCombinations = [
     [0, 1, 2],
@@ -15,12 +17,18 @@ const winningCombinations = [
 ];
 
 function handleCellClick(e) {
+    // X o O
     const cell = e.target;
-    const index = cell.getAttribute('data-index');
-
+    // posicion
+    const index = parseInt(cell.getAttribute('data-index'));
+    const position = index + 1;
+    modificar_lista(currentPlayer, position)
+   
     if (board[index] === '') {
         board[index] = currentPlayer;
         cell.textContent = currentPlayer;
+        console.log(`Jugador ${currentPlayer} ha jugado en la posición ${position}`);
+
         if (checkWin(currentPlayer)) {
             alert(`Jugador ${currentPlayer} gana!`);
             resetGame();
@@ -54,3 +62,37 @@ cells.forEach(cell => {
 });
 
 resetButton.addEventListener('click', resetGame);
+
+//modelo
+function modificar_lista(valor, posicion) {
+    tf.ready().then(() => {
+        console.log(posicion, valor)
+
+        const modelPath = './ttt_model.json'
+        tf.tidy(() => {
+            tf.loadLayersModel(modelPath).then((model) => {
+                let nuevoValor;
+
+                if (valor === "X") {
+                    nuevoValor = -1;
+                } else if (valor === "O") {
+                    nuevoValor = 1;
+                } else {
+                    console.log("Ocurrio un error")
+                    return;
+                }
+
+                const nuevoG = g.arraySync(); // Obtenemos una copia del tensor como un array
+                nuevoG[posicion-1] = nuevoValor; // Actualizamos el valor en el array según la posición
+
+                g = tf.tensor(nuevoG); // Convertimos el array actualizado de nuevo a un tensor
+                console.log('Nuevo valor de g:', g.arraySync());
+
+                const matches = tf.stack([g])
+                const result = model.predict(matches)
+                // El resultado final del juego
+                result.reshape([9]).print()
+            })
+        })
+    })
+}
